8415d6ead92fab5003cd8f748240a17eaa7cbd7e
x2020chao <x2020chao@163.com>
2022-03-01 17:27:19 +0800

    mediator

---
+++ src/test/java/com/example/demo/mode/mediator/TestLoginFrame.java
@@ -0,0 +1,9 @@
+package com.example.demo.mode.mediator;
+
+import com.example.demo.mode.mediator.concreate.LoginFrame;
+
+public class TestLoginFrame {
+    public static void main(String[] args) {
+        new LoginFrame("Mediator Sample");
+    }
+}
